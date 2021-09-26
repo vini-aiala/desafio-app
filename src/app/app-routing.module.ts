085cedc62346a5a1c6e9c35dd0475ba0501196af
Vinícius Aiala <vinicius_aiala@outlook.com>
2021-09-26 19:47:45 -0300

    App routing endpoints

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {SubjectsListComponent} from "./subjects/subjects-list/subjects-list.component";
+import {QuestionsListComponent} from "./questions/questions-list/questions-list.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"", component:SubjectsListComponent},
+  {path:"subject/:subject", component:QuestionsListComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,13 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {UiModule} from "./ui/ui.module";
+import {MatToolbarModule} from "@angular/material/toolbar";
+import {SubjectsModule} from "./subjects/subjects.module";
+import {QuestionsModule} from "./questions/questions.module";
 
 @NgModule({
   declarations: [
@@ -10,7 +15,12 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    UiModule,
+    MatToolbarModule,
+    SubjectsModule,
+    QuestionsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
