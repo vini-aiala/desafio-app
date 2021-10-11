ab6638dd54e3395ef62a2ad7bfecb247a735f359
Vinícius Aiala <vinicius_aiala@outlook.com>
2021-10-11 15:25:02 -0300

    Generated form components

---
+++ src/app/answers/answers-form/answers-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-answers-form',
+  templateUrl: './answers-form.component.html',
+  styleUrls: ['./answers-form.component.css']
+})
+export class AnswersFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
